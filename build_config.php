1b6340b34f235ea5d71c0c16e29ab6cbfe9bd16c
Aaron Crawford <info@alleghenycreative.com>
2019-06-24 20:55:58 -0400

    Add build_config.php

---
+++ build_config.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* This is the file that gets called for this module when OpenRepeater rebuilds the configuration files for SVXLink.
+* Settings for the config file are created as a PHP associative array, when the file is called it will convert it into
+* the requiried INI format and write the config file to the appropriate location with the correct naming.
+*/
+
+// Build Config
+$module_config_array['Module'.$cur_mod['svxlinkName']] = [
+	'NAME' => $cur_mod['svxlinkName'],
+	'PLUGIN_NAME' => 'Tcl',
+	'ID' => $cur_mod['svxlinkID'],
+];
+
+$module_config_array['Module'.$cur_mod['svxlinkName']] += [
+	'RIGCTL_1' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 1"',
+	'RIGCTL_2' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 2"',
+	'RIGCTL_3' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 3"',
+	'RIGCTL_4' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 4"',
+	'RIGCTL_5' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 5"',
+	'RIGCTL_6' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 6"',
+	'RIGCTL_7' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 7"',
+	'RIGCTL_8' => '"-m 370 -r /dev/ttyUSB0 -s 9600 E 8"',
+];
+
+?>
